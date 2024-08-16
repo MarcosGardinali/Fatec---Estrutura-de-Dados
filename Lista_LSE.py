eeeb73ff70999d878c715608fa7795afe2d564e6
MarcosGardinali <marcosgardinali21@outlook.com>
2024-08-16 20:27:07 -0300

    Inserção de novas funcionalidades ao algoritmo de LSE

--- Lista_LSE.py
+++ Lista_LSE.py
@@ -1,8 +1,7 @@
-class no:
+class No:
     def __init__(self, val):
         self.info = val
         self.prox = None
-
 
     def getInfo(self):
         return self.info
@@ -15,3 +14,55 @@ class no:
 
     def setProx(self, proximo):
         self.prox = proximo
+
+class LSE:
+    def __init__(self):
+        self.Inicio = None
+
+    def Ins_Inicio(self, val):
+        p = No(val)
+        p.setProx(self.Inicio)
+        self.Inicio = p
+
+    def Ins_Fim(self, val):
+        p = No(val)
+        if(self.Inicio == None):
+            self.Inicio = p
+        else:
+            q = self.Inicio
+            while q.getProx() != None:
+                q = q.getProx()
+                
+            q.setProx(p)
+
+    def Imprime(self):
+        p = self.Inicio
+        while p != None:
+            print(p.getInfo(), "--> ", end ='')
+            p = p.getProx()
+
+        print("None")
+
+L = LSE()
+
+while True:
+    print("1 - Para inserir um valor ao início!")
+    print("2 - Para imprimir a lista!")
+    print("3 - Inserir um valor ao final!")
+    print("0 - Para sair!")
+
+    op = int(input("\nDigite a opção: \n"))
+
+    if(op == 0):
+        break
+    if(op == 1):
+        val = int(input("\nInsira o valor que deseja inserir: "))
+        L.Ins_Inicio(val)
+    if(op == 2):
+        if(L.Inicio == None):
+            print("\nLista Vazia!\n")
+        else:
+            L.Imprime()
+    if(op == 3):
+        val = int(input("\nInsira o valor que deseja inserir: "))
+        L.Ins_Fim(val)
